a3d09f7ecdaab11a135257265eaf2dd57cbcdc1f
Jeremiah Orem <oremj@oremj.com>
2012-12-10 14:23:19 -0800

    initial commit

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,67 @@
+class nginx(
+    $nx_user = 'nginx',
+    $version = '1.2.3-1.el6.ngx'
+){
+    realize(Yumrepo['nginx'])
+    package {
+        'nginx':
+            require => Yumrepo['nginx'],
+            ensure  => $version;
+    }
+
+    service {
+        'nginx':
+            require    => Package['nginx'],
+            ensure     => running,
+            enable     => true,
+            hasstatus  => true,
+            hasrestart => true;
+    }
+
+    file {
+        # the absence of 'source => ...' tells puppet that we want to remove all unmanaged files from these directories.
+        # TODO: Fix bug 811515 someday, so that unmanaged directories are removed as well.
+        ['/etc/nginx/',
+         '/etc/nginx/conf.d/',
+         '/etc/nginx/managed/']:
+            notify  => Service['nginx'],
+            ensure  => 'directory',
+            force   => true,
+            recurse => true,
+            purge   => true;
+
+        '/etc/nginx/nginx.conf':
+            content => template('nginx/nginx.conf');
+
+        '/etc/nginx/conf.d/managed.conf':
+            content => "include managed/*.conf;\n";
+
+        '/etc/nginx/mime.types':
+            source => 'puppet:///modules/nginx/mime.types';
+
+        '/etc/nginx/conf.d/hostname.conf':
+            require => Package[nginx],
+            before  => Service[nginx],
+            content => "add_header X-Backend-Server ${fqdn};\n";
+
+        '/etc/sysconfig/nginx':
+            require => Package['nginx'],
+            mode    => '0644',
+            content => template('nginx/sysconfig/nginx');
+
+        '/etc/logrotate.d/nginx':
+            require => Package[nginx],
+            owner   => $user,
+            group   => root,
+            mode    => '0644',
+            content => template('nginx/logrotate.conf');
+
+        '/etc/init.d/nginx':
+            require => Package[nginx],
+            before  => Service[nginx],
+            owner   => root,
+            group   => root,
+            mode    => '0755',
+            source  => 'puppet:///modules/nginx/etc-init.d/nginx';
+    }
+}
---
+++ modules/nginx/manifests/logdir.pp
@@ -0,0 +1,11 @@
+# Helper for creating "/var/log/nginx/<domainname>"
+define nginx::logdir {
+    file { "/var/log/nginx/${name}":
+        ensure  => directory,
+        require => Package['nginx'],
+        owner   => $nginx::nx_user,
+        group   => 'root',
+        mode    => '0755',
+        purge   => true,
+    }
+}
