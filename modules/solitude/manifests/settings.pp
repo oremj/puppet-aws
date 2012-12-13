82ca3695e808c88fb0e4ff7fc9e261f9d051adfc
Jeremiah Orem <oremj@oremj.com>
2012-12-13 13:59:07 -0800

    solitude worker and settings.

---
+++ modules/solitude/manifests/settings.pp
@@ -0,0 +1,17 @@
+define solitude::settings(
+    $project_dir,
+    $db_host,
+    $db_name,
+    $db_user,
+    $db_password,
+    $secret_key
+    $db_port => '3306',
+) {
+
+    file {
+        "${project_dir}/solitude/settings/local.py":
+            content => template('solitude/settings/local.py');
+    }
+    
+
+}
---
+++ modules/solitude/manifests/worker.pp
@@ -0,0 +1,14 @@
+define solitude::worker(
+    $project_dir,
+    $workers => 4,
+    $settings_module => 'solitude.settings.local'
+) {
+    $app_name = $name
+    supervisord::program {
+        "gunicorn-solitude-${project_dir}":
+            command => "${project_dir}/venv/bin/gunicorn -w ${workers} --max-requests 5000 -n gunicorn-$app_name wsgi.playdoh:application",
+            cwd => "${project_dir}/solitude",
+            user => "nginx",
+            environ => "DJANGO_SETTINGS_MODULE=${settings_module}";
+    }
+}
