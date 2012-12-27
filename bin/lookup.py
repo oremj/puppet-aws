65a6bc390990f25dc5fb4f0a4d782ce78bb2e643
Jason Thomas <jason@lithiumfox.com>
2012-12-27 13:35:36 -0500

    create config file

--- bin/lookup.py
+++ bin/lookup.py
@@ -4,10 +4,17 @@
 from boto.ec2 import connect_to_region, regions
 import sys
 import yaml
+import os
+import ConfigParser
 
-region = 'us-west-2'
-aws_access_key_id = ''
-aws_secret_access_key = ''
+config_file =  "%s/.lookup.cfg" %  os.path.dirname(os.path.realpath(__file__))
+config = ConfigParser.RawConfigParser()
+config.read(config_file)
+
+region = config.get('aws', 'region')
+aws_id = config.get('aws', 'id')
+aws_secret = config.get('aws', 'secret')
+
 fqdn = sys.argv[1]
 filters = {'tag:Project': 'amo',
            'private_dns_name': fqdn,
@@ -15,8 +22,8 @@ filters = {'tag:Project': 'amo',
 
 def get_type(filters):
     try:
-        conn = connect_to_region(region, aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key)
+        conn = connect_to_region(region, aws_access_key_id=aws_id,
+                                 aws_secret_access_key=aws_secret)
         reservations = conn.get_all_instances(filters=filters)
         if not reservations:
             sys.exit('Host not found')
@@ -24,6 +31,7 @@ def get_type(filters):
             return reservations[0].instances[0].tags
     except:
         print sys.exc_info()
+        sys.exit(1)
 
 def create_yaml(tags):  
     tclass = "%s::%s::%s" % (tags['App'], tags['Type'], tags['Env'])
