61d405d17aaf3bcea9c576d38b885be019af9234
Jason Thomas <jason@lithiumfox.com>
2012-12-27 17:00:25 -0500

    write to temp file

--- bin/autosign.py
+++ bin/autosign.py
@@ -2,10 +2,10 @@
 ## populate /etc/puppet/autosign.conf based on tags
 
 from boto.ec2 import connect_to_region, regions
+from shutil import move
 import sys
 import os
 import ConfigParser
-
 
 config_file = "%s/.aws.cfg" % os.path.dirname(os.path.realpath(__file__))
 config = ConfigParser.RawConfigParser()
@@ -16,6 +16,7 @@ aws_id = config.get('aws', 'id')
 aws_secret = config.get('aws', 'secret')
 
 autosign = '/etc/puppet/autosign.conf'
+temp = '%s.tmp' % autosign
 filters = {'tag:Project': 'amo'}
 
 try:
@@ -25,8 +26,10 @@ try:
     private_dns = [i.private_dns_name for r in reservations
                    for i in r.instances]
 
-    with open(autosign, 'w') as fp:
+    private_dns = filter(None, private_dns)
+    with open(temp, 'w') as fp:
         for i in private_dns:
             fp.write(i + '\n')
+        move(temp, autosign)
 except:
     print sys.exc_info()
