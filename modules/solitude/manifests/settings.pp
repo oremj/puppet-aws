9ee20021a9c6693f4d659ff4fcef2961888bb569
Jeremiah Orem <oremj@oremj.com>
2012-12-31 14:45:39 -0800

    More solitude settings.

--- modules/solitude/manifests/settings.pp
+++ modules/solitude/manifests/settings.pp
@@ -2,6 +2,15 @@ define solitude::settings(
     $project_dir,
     $db_url,
     $secret_key,
+    $server_email,
+    $email_host,
+    $memcache_hosts,
+    $cache_prefix,
+    $broker_url,
+    $solitude_proxy,
+    $sentry_dsn,
+    $paypal_url_whitelist,
+    $aes_key_dir
 ) {
 
     file {
--- modules/solitude/templates/settings/local.py
+++ modules/solitude/templates/settings/local.py
@@ -4,8 +4,9 @@
 # To extend any settings from settings/base.py here's an example:
 import dj_database_url
 from . import base
-INSTALLED_APPS = base.INSTALLED_APPS + ('django_extensions',)
-#INSTALLED_APPS = base.INSTALLED_APPS + ('debug_toolbar')
+
+SERVER_EMAIL = '<%= server_email %>'
+EMAIL_HOST = '<%= email_host %>'
 
 DATABASES = {
     'default': dj_database_url.parse('<%= db_url %>')
@@ -23,6 +24,16 @@ DATABASES['default'].update({
 
 # Uncomment this and set to all slave DBs in use on the site.
 # SLAVE_DATABASES = ['slave']
+CACHE_PREFIX = '<% cache_prefix %>'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '<%= memcache_hosts %>'.split(';'),
+        'TIMEOUT': 500,
+        'KEY_PREFIX': CACHE_PREFIX,
+    },
+}
 
 # Recipients of traceback emails and other notifications.
 ADMINS = (
@@ -49,13 +60,31 @@ PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
 SECRET_KEY = '<%= secret_key %>'
 
 # Uncomment these to activate and customize Celery:
-# CELERY_ALWAYS_EAGER = False  # required to activate celeryd
-# BROKER_HOST = 'localhost'
-# BROKER_PORT = 5672
-# BROKER_USER = 'playdoh'
-# BROKER_PASSWORD = 'playdoh'
-# BROKER_VHOST = 'playdoh'
-# CELERY_RESULT_BACKEND = 'amqp'
+CELERY_ALWAYS_EAGER = False  # required to activate celeryd
+BROKER_URL = '<%= broker_url %>'
+
+CELERY_RESULT_BACKEND = 'database'
+CELERY_RESULT_DBURI = '<%= db_url %>'
+
+SYSLOG_TAG = 'http_app_<%= cache_prefix %>'
+
+CLEANSED_SETTINGS_ACCESS = True
+
+PAYPAL_PROXY = '<%= solitude_proxy %>/proxy/paypal/'
+
+SENTRY_DSN = '<%= sentry_dsn %>'
+
+PAYPAL_LIMIT_PREAPPROVAL = False
+PAYPAL_URL_WHITELIST = ('<%= paypal_url_whitelist %>',)
+
+AES_KEYS = {
+    'buyerpaypal:key': '<%= aes_key_dir %>/buyerpaypal.key',
+    'sellerbluevia:id': '<%= aes_key_dir %>/sellerbluevia.key',
+    'sellerpaypal:id': '<%= aes_key_dir %>/sellerpaypal_id.key',
+    'sellerpaypal:token': '<%= aes_key_dir %>/sellerpaypal_token.key',
+    'sellerpaypal:secret': '<%= aes_key_dir %>/sellerpaypal_secret.key',
+    'sellerproduct:secret': '<%= aes_key_dir %>/sellerproduct_secret.key',
+}
 
 ## Log settings
 
