ff29e5d90e3f91dfcbadf94e5d4d9ef50c1183cb
Jason Thomas <jason@lithiumfox.com>
2013-01-02 23:54:01 -0500

    enable epel

---
+++ modules/base/manifests/yum.pp
@@ -0,0 +1,11 @@
+class base::yum {
+
+  yumrepo { "epel":
+      mirrorlist     => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch",
+      descr          => "Extra Packages for Enterprise Linux 6 - $basearch",
+      enabled        => 1,
+      gpgcheck       => 1,
+      failovermethod => priority,
+      gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+  }
+}
