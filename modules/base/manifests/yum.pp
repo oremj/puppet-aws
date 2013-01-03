b92f2dad30fa921ea0f436d8dad7fb50e90e4487
Jason Thomas <jason@lithiumfox.com>
2013-01-02 23:56:30 -0500

    single quotes

--- modules/base/manifests/yum.pp
+++ modules/base/manifests/yum.pp
@@ -1,8 +1,8 @@
 class base::yum {
 
   yumrepo { "epel":
-      mirrorlist     => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch",
-      descr          => "Extra Packages for Enterprise Linux 6 - $basearch",
+      mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+      descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
       enabled        => 1,
       gpgcheck       => 1,
       failovermethod => priority,
