833d445149845e64aecee8ec22205e5fd68e21ea
Jason Thomas <jason@lithiumfox.com>
2013-01-08 17:39:39 -0500

    download package to host and then install

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -3,12 +3,17 @@ class nodejs (
 ){
 
   $node_rpm = "nodejs-${version}-1.x86_64.rpm"
+  file {
+    "/var/tmp/${node_rpm}":
+      ensure => present,
+      source   => "puppet:///modules/${module_name}/${node_rpm}",
+  }
 
   package {
     'nodejs':
       provider => rpm,
       ensure   => present,
-      source   => "puppet:///modules/${module_name}/${node_rpm}",
+      source   => "file:///var/tmp/${node_rpm}",
   }
 
 }
