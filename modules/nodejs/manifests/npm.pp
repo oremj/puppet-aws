207b26ed55a7d7c8f8a2a1330888ced9b2a03ce2
Jason Thomas <jason@lithiumfox.com>
2013-01-08 17:21:48 -0500

    fix requires

--- modules/nodejs/manifests/npm.pp
+++ modules/nodejs/manifests/npm.pp
@@ -5,7 +5,7 @@ define nodejs::npm {
   exec { "npm-${name}":
     command  => "/usr/bin/npm install -g ${name} && /root/.npm.${name}",
     creates  => "/root/.npm.${name}",
-    requires => Package['nodejs'],
+    requires => Class['nodejs'];
   }
 
 }
