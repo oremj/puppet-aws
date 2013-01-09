852422a451cfe83130250c6d6e9b02ffba31fb22
Jason Thomas <jason@lithiumfox.com>
2013-01-09 18:24:59 -0500

    statsd needs nodejs

--- modules/statsd/manifests/init.pp
+++ modules/statsd/manifests/init.pp
@@ -2,6 +2,8 @@ class statsd (
   $version='0.5.0-1'
 ){
 
+  include nodejs
+
   $user = 'statsd'
   $statsd_rpm = "nodejs-statsd-${version}.amzn1.x86_64.rpm"
   file {
@@ -15,7 +17,10 @@ class statsd (
       provider => rpm,
       ensure   => present,
       source   => "file:///var/tmp/${statsd_rpm}",
-      require  => File["/var/tmp/${statsd_rpm}"],
+      require  => [
+                    File["/var/tmp/${statsd_rpm}"],
+                    Class['nodejs'],
+      ],
   }
 
   base::user{
