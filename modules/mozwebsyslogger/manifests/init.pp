cc67c0733e3e2f5694e88b80be03cead2fd90e5b
Jeremiah Orem <oremj@oremj.com>
2013-01-09 16:48:25 -0800

    Add more logging to syslog host.

--- modules/mozwebsyslogger/manifests/init.pp
+++ modules/mozwebsyslogger/manifests/init.pp
@@ -3,13 +3,15 @@ class mozwebsyslogger {
     include rsyslog::udpserver
 
     file {
-        '/var/log/clusterlogs':
+        ['/var/log/clusterlogs',
+         '/var/log/clusterlogs/hosts']:
             mode => '0755',
             ensure => directory;
     }
 
     rsyslog::config {
         'websyslogger':
+            require => File['/var/log/clusterlogs/hosts'],
             content => template('mozwebsyslogger/syslog.conf');
     }
 }
