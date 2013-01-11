a057d609f633b2f4f392ba2f3dff853a71d2112c
Jeremiah Orem <oremj@oremj.com>
2013-01-11 09:27:48 -0800

    Cut down fabfile.

--- modules/solitude/templates/fabfile.py
+++ modules/solitude/templates/fabfile.py
@@ -1,12 +1,10 @@
 import os
-import re
-import time
 from functools import partial
 
-from fabric.api import lcd, local, task
+from fabric.api import local, task
 
-from mozawsdeploy import config, ec2
-from mozawsdeploy.fabfile import aws
+from mozawsdeploy import ec2
+from mozawsdeploy.fabfile import aws, web
 
 
 PROJECT_DIR = os.path.normpath(os.path.dirname(__file__))
@@ -14,6 +12,8 @@ PROJECT_DIR = os.path.normpath(os.path.dirname(__file__))
 AMAZON_AMI = 'ami-2a31bf1a'
 SUBNET_ID = '<%= subnet_id %>'
 ENV = '<%= site %>'
+
+SERVER_TYPES = ['syslog', 'celery', 'sentry', 'rabbitmq', 'graphite']
 
 create_server = partial(aws.create_server, app='solitude', ami=AMAZON_AMI,
                         subnet_id=SUBNET_ID, env=ENV)
@@ -34,56 +34,16 @@ def create_web(instance_type='m1.small', count=1):
 
 
 @task
-def create_syslog(instance_type='m1.small'):
+def create_instance(server_type, instance_type='m1.small'):
     """
-    args: instance_type
+    args: server_type, instance_type.
+          Valid server_types are listed in SERVER_TYPES
     """
-    instances = create_server(server_type='syslog',
+    assert server_type in SERVER_TYPES
+
+    instances = create_server(server_type=server_type,
                               instance_type=instance_type)
-
-
-@task
-def create_celery(instance_type='m1.small'):
-    """
-    args: instance_type
-    This function will create the "golden master" ami for solitude web servers.
-    TODO: needs to user_data to puppetize server
-    """
-    instances = create_server(server_type='celery',
-                              instance_type=instance_type)
-
-
-@task
-def create_sentry(instance_type='m1.small'):
-    """
-    args: instance_type
-    This function will create the "golden master" ami for solitude web servers.
-    TODO: needs to user_data to puppetize server
-    """
-    instances = create_server(server_type='sentry',
-                              instance_type=instance_type)
-
-
-@task
-def create_rabbitmq(instance_type='m1.small'):
-    """
-    args: instance_type
-    This function will create the "golden master" ami for solitude web servers.
-    TODO: needs to user_data to puppetize server
-    """
-    instances = create_server(server_type='rabbitmq',
-                              instance_type=instance_type)
-
-
-@task
-def create_graphite(instance_type='m1.small'):
-    """
-    args: instance_type
-    This function will create the "golden master" ami for solitude web servers.
-    TODO: needs to user_data to puppetize server
-    """
-    instances = create_server(server_type='graphite',
-                              instance_type=instance_type)
+    return instances
 
 
 @task
@@ -110,39 +70,10 @@ def create_security_groups():
 @task
 def build_release(ref):
     """Build release. This assumes puppet has placed settings in /settings"""
-    project_dir = PROJECT_DIR
-    release_time = time.time()
-    release_id = 'solitude-%d-%s' % (release_time, re.sub('[^A-z0-9]',
-                                                          '.', ref))
-    release_dir = os.path.join(project_dir, 'builds', release_id)
-    tarball = os.path.join(project_dir, 'releases', '%s.tar.gz' % release_id)
+    def extra(release_dir):
+        local('rsync -av %s/aeskeys/ %s/aeskeys/' % (PROJECT_DIR, release_dir))
 
-    local('mkdir -p %s' % release_dir)
-    local('git clone git://github.com/mozilla/solitude.git %s/solitude'
-          % release_dir)
-    with lcd('%s/solitude' % release_dir):
-        local('git reset --hard %s' % ref)
-
-    local('virtualenv --distribute --never-download %s/venv' % release_dir)
-    local('%s/venv/bin/pip install --exists-action=w --no-deps '
-          '--no-index --download-cache=/tmp/pip-cache '
-          '-f https://pyrepo.addons.mozilla.org '
-          '-r %s/solitude/requirements/prod.txt' %
-          (release_dir, release_dir))
-    local('rm -f %s/venv/lib/python2.6/no-global-site-packages.txt' %
-          release_dir)
-    local('%s/venv/bin/python /usr/bin/virtualenv --relocatable %s/venv' %
-          (release_dir, release_dir))
-
-    local('rsync -av %s/settings/ %s/solitude/solitude/settings/' %
-          (project_dir, release_dir))
-
-    local('rsync -av %s/aeskeys/ %s/aeskeys/' % (project_dir, release_dir))
-
-    local('tar czf %s -C %s solitude aeskeys venv' %
-          (tarball, release_dir))
-
-    with lcd(project_dir):
-        local('ln -snf %s/solitude solitude' % release_dir)
-        local('ln -snf %s/venv venv' % release_dir)
-        local('ln -snf %s latest.tar.gz' % tarball)
+    web.build_release('solitude', PROJECT_DIR,
+                      repo='git://github.com/mozilla/solitude.git', ref=ref,
+                      requirements='requirements/prod.txt',
+                      settings_dir='solitude/settings', extra=extra)
