61a7f9446a14f67ab2f2cb9f30bcf917468ed1d7
Jason Thomas <jason@lithiumfox.com>
2013-01-15 17:25:42 -0500

    add ebs volume when host starts

--- modules/mozwebsyslogger/manifests/init.pp
+++ modules/mozwebsyslogger/manifests/init.pp
@@ -1,12 +1,23 @@
-class mozwebsyslogger {
+class mozwebsyslogger(
+    $device = '/dev/xvdf1'
+){
     include rsyslog
     include rsyslog::udpserver
+    
+    mount { 
+        '/var/log/clusterlogs':
+            device  => "${device}",
+            fstype  => 'ext4',
+            ensure  => 'mounted',
+            options => 'defaults',
+            atboot  => 'true',
+    }
 
     file {
-        ['/var/log/clusterlogs',
-         '/var/log/clusterlogs/hosts']:
-            mode => '0755',
-            ensure => directory;
+         ['/var/log/clusterlogs/hosts']:
+            mode    => '0755',
+            ensure  => directory,
+            require => Mount['/var/log/clusterlogs'];
     }
 
     rsyslog::config {
