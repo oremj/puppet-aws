18ea4f3bc8afff3620a2e2ed7ce693c5cc9a01b3
Jason Thomas <jason@lithiumfox.com>
2013-01-16 17:03:08 -0500

    break out rsyslog from mozwebnode

--- modules/mozwebnode/manifests/init.pp
+++ modules/mozwebnode/manifests/init.pp
@@ -1,20 +1,9 @@
-class mozwebnode(
-    $syslog_servers,
-    $rsyslog_ca_cert_content,
-    $rsyslog_ca_cert = '/etc/pki/rsyslog/rsyslog-ca.crt',
-    $tls = true
-){
+class mozwebnode{
     include supervisord
-    include rsyslog
 
     package {
         'git':
             ensure => 'present';
-    }
-
-    rsyslog::config {
-        'web':
-            content => template('mozwebnode/syslog.conf');
     }
 
     class {
@@ -25,17 +14,6 @@ class mozwebnode(
     file {
         '/data':
             ensure => 'directory';
-    }
-
-    # install ca cert
-    if $tls {
-      file {
-        "${rsyslog_ca_cert}":
-          ensure  => present,
-          mode    => '0644',
-          content => template("${rsyslog_ca_cert_content}"),
-          before  => Rsyslog::Config['web'],
-      }
     }
 
 }
---
+++ modules/rsyslog/manifests/node.pp
@@ -0,0 +1,25 @@
+class rsyslog::node (
+    $syslog_servers,
+    $rsyslog_ca_cert_content,
+    $rsyslog_ca_cert = '/etc/pki/rsyslog/rsyslog-ca.crt',
+    $tls = true
+){
+    include rsyslog
+
+    rsyslog::config {
+        'node':
+            content => template('rsyslog/syslog.conf');
+    }
+
+    # install ca cert
+    if $tls {
+      file {
+        "${rsyslog_ca_cert}":
+          ensure  => present,
+          mode    => '0644',
+          content => template("${rsyslog_ca_cert_content}"),
+          before  => Rsyslog::Config['web'],
+      }
+    }
+
+}
