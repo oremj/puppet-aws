1eee5f9af0ba4e61b2f9411260472343700fff6b
Jason Thomas <jason@lithiumfox.com>
2013-02-13 14:59:54 -0500

    remove pam  before install moz packages

--- modules/audit/manifests/aws.pp
+++ modules/audit/manifests/aws.pp
@@ -24,10 +24,10 @@ class audit::aws {
             before => Package['audit-libs'];
         'audit-libs-2.2-2.17.amzn1.i686':
             ensure => absent,
-            before => Package['audit_package'];
+            before => [Package['audit-mozilla-libs'], Package['audit_package']];
         'pam-1.1.1-10.19.amzn1.i686':
             ensure => absent,
-            before => Package['audit_package'];
+            before => [Package['audit-mozilla-libs'], Package['audit_package']];
         'audispd-plugins-1.8-3.el5.centosmoz5.i386':
             ensure => absent;
     }
