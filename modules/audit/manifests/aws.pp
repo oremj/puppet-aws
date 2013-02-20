5f383232971b4871f413d1214a687534927e3d3e
Jason Thomas <jason@lithiumfox.com>
2013-02-20 10:50:26 -0500

    fix auditd dependency

--- modules/audit/manifests/aws.pp
+++ modules/audit/manifests/aws.pp
@@ -1,34 +1,33 @@
 class audit::aws {
+    # remove these before we even start.
     package {
-        'audit_package':
-            ensure => latest,
-            name   => 'audit-mozilla';
+      [
+        'audit',
+        'audispd-plugins',
+        'audit-libs-python',
+        'audit-libs-2.2-2.17.amzn1.i686',
+        'pam-1.1.1-10.19.amzn1.i686',
+      ]:
+            ensure => absent,
+            before => [ 
+                        Package['audispd-mozilla-plugins'],
+                        Package['audit-mozilla'],
+                        Package['audit-mozilla-libs'],
+                        Package['audit-mozilla-libs-python'],
+            ];
+    }
+
+    package {
+        'audit-mozilla':
+            ensure => latest;
         'audispd-mozilla-plugins':
             ensure => latest;
         'audit-mozilla-libs-python':
             ensure => latest;
         'audit-mozilla-libs':
             ensure => latest,
-            before => Package['audit_package'];
-
-        'audit':
-            ensure => absent,
-            before => Package['audit_package'];
-        'audispd-plugins':
-            ensure => absent,
-            before => Package['audispd-mozilla-plugins'];
-        'audit-libs-python':
-            ensure => absent;
+            before => Package['audit-mozilla'];
         'audit-libs':
-            ensure => latest,
-            before => Package['audit-libs'];
-        'audit-libs-2.2-2.17.amzn1.i686':
-            ensure => absent,
-            before => [Package['audit-mozilla-libs'], Package['audit_package']];
-        'pam-1.1.1-10.19.amzn1.i686':
-            ensure => absent,
-            before => [Package['audit-mozilla-libs'], Package['audit_package']];
-        'audispd-plugins-1.8-3.el5.centosmoz5.i386':
-            ensure => absent;
+            ensure => latest;
     }
 }
