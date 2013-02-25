789a2af6cfae7cccdcd5a91e574863e6420fa9f7
Jason Thomas <jason@lithiumfox.com>
2013-02-25 10:44:31 -0500

    add cron to clean up carbon-cache relay log directory

--- modules/graphite/manifests/carbon/service.pp
+++ modules/graphite/manifests/carbon/service.pp
@@ -1,7 +1,5 @@
 class graphite::carbon::service {
 
-
-
   service { 'carbon-cache':
     ensure     => running,
     enable     => true,
@@ -10,7 +8,13 @@ class graphite::carbon::service {
     require    => Package['carbon'],
   }
 
-
+  cron {
+      'cleanup-carbon-cache':
+          command => '/bin/find /var/log/carbon-cache -type f -mtime +1 -name "*log*" -delete > /dev/null 2>&1',
+          user    => root,
+          minute  => '1',
+          hour    => '2';
+  }
 }
 
 
