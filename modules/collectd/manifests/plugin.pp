12a57554a86d3b832164fc5d0e42a8580d731178
Jason Thomas <jason@lithiumfox.com>
2013-03-06 13:20:23 -0500

    add plugin support for collectd

---
+++ modules/collectd/manifests/plugin.pp
@@ -0,0 +1,22 @@
+define collectd::plugin(
+  $package = false
+){
+  include collectd
+
+  $include_dir = '/etc/collectd.d'
+
+  if $package {
+      package {
+        "collectd-${name}":
+            ensure => latest;
+      }
+  }
+
+  file {
+    "${include_dir}/${name}.conf":
+        ensure  => present,
+        content => template('collectd/collectd.d/${name}.conf.erb'),
+        notify  => Service['collectd'];
+  }
+
+}
