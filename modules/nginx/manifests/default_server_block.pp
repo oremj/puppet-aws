c7c63efe9becf0e37e47c6eb52b425e25c9b442a
Jeremiah Orem <oremj@oremj.com>
2013-03-07 13:27:30 -0800

    Add default vhost.

---
+++ modules/nginx/manifests/default_server_block.pp
@@ -0,0 +1,10 @@
+define nginx::default_server_block(
+    $content
+) {
+    $conf_name = $name
+    nginx::config {
+        "${conf_name}_default":
+            content => $content,
+            suffix  => '.default';
+    }
+}
