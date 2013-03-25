1268c3be972f279b3d5fdcdfa422890bfb80c154
Jason Thomas <jason@lithiumfox.com>
2013-03-25 12:24:20 -0400

    Manage sysconfig/network file and set hostname

--- modules/hostname/manifests/init.pp
+++ modules/hostname/manifests/init.pp
@@ -5,4 +5,10 @@ class hostname(
         "/bin/hostname ${hostname}":
             unless => "/usr/bin/test \"$(/bin/hostname)\" = \"${hostname}\"";
     }
+
+    file {
+        '/etc/sysconfig/network':
+            ensure  => present,
+            content => template('hostname/network.erb');
+    }
 }
