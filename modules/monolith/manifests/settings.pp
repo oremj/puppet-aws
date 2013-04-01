d5ee8fd3b46ed6b2409b15bf34bb44141f8c8f26
Jeremiah Orem <oremj@oremj.com>
2013-04-01 14:49:12 -0700

    Simplify monolith settings.

--- modules/monolith/manifests/settings.pp
+++ modules/monolith/manifests/settings.pp
@@ -1,5 +1,9 @@
 # Installs settings file for monolith.
 define monolith::settings(
+    $lb_name,
+    $subnet_id,
+    $instance_type => 'm1.small',
+    $cluster = 'monolith',
     $elasticsearch_hosts = 'localhost:9200',
     $statsd_host = 'localhost',
     $statsd_port = '8125',
@@ -7,9 +11,26 @@ define monolith::settings(
     $statsd_prefix = 'monolith.web',
     $settings_template = 'monolith/monolith.ini'
 ){
-    $settings_dir = $name
+    $site_name = $name
+    $project_dir = "/data/${cluster}/www/${site_name}"
+    $settings_dir = "${project_dir}/settings"
+
+    file {
+        "${project_dir}/fabfile.py":
+            content => template('monolith/fabfile.py');
+    }
+
     file {
         "${settings_dir}/monolith.ini":
             content => template($settings_template);
+    }
+
+    mozwebadmin::application {
+        $site_name:
+            project_dir => $project_dir,
+            lb_name => $lb_name,
+            subnet_id => $subnet_id,
+            cluster => $cluster,
+            instance_type => $instance_type;
     }
 }
