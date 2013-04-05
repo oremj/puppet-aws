afdeab1a0f5ab43dc0c57d13bb96013b17b4b74e
Jeremiah Orem <oremj@oremj.com>
2013-04-05 14:14:03 -0700

    Use new gunicorn class for gunicorn.

---
+++ modules/gunicorn/manifests/process.pp
@@ -0,0 +1,27 @@
+# Set up gunicorn process
+define gunicorn::process(
+    $gunicorn,
+    $port,
+    $wsgi_module,
+    $cwd,
+    $user,
+    $addr = '127.0.0.1',
+    $workers = '4',
+    $max_requests = '5000',
+    $log_syslog = true
+) {
+    $app_name = $name
+
+    $log_syslog_arg = $log_syslog ? {
+        true => '--log-syslog',
+        false => '',
+    }
+
+    supervisord::program {
+        "gunicorn-${app_name}":
+            command => "${gunicorn} -b ${addr}:${port} -w ${workers} --max-requests ${max_requests} -n gunicorn-${app_name} ${log_syslog_arg} ${wsgi_module}",
+            cwd     => $cwd,
+            user    => 'nginx';
+    }
+
+}
--- modules/monolith/manifests/worker.pp
+++ modules/monolith/manifests/worker.pp
@@ -6,11 +6,14 @@ define monolith::worker(
     $workers = 4,
 ) {
     $app_name = $name
-    supervisord::program {
-        "gunicorn-monolith-${app_name}":
-            command => "${project_dir}/venv/bin/gunicorn -b 127.0.0.1:${port} -w ${workers} --max-requests 5000 -n gunicorn-${app_name} ${wsgi_module}",
-            cwd     => "${project_dir}/monolith",
-            user    => 'nginx';
+    gunicorn::process {
+        "monolith-${app_name}":
+            gunicorn     => "${project_dir}/venv/bin/gunicorn",
+            port         => $port,
+            wsgi_module  => $wsgi_module,
+            workers      => $workers,
+            user         => 'nginx',
+            cwd          => "${project_dir}/monolith";
     }
 
     nginx::upstream {
