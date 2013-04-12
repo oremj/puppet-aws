bd22718c70fd1049c50b6f06a1daf43e3cfabb2e
Jason Thomas <jason@lithiumfox.com>
2013-04-12 12:55:46 -0400

    log output of aggregator cron

--- modules/monolith/manifests/aggregator_instance.pp
+++ modules/monolith/manifests/aggregator_instance.pp
@@ -5,7 +5,7 @@ define monolith::aggregator_instance(
     $aggr_name = $name
     cron {
         $aggr_name:
-            command => "cd ${project_dir}/current/; ./venv/bin/monolith-extract --date yesterday configs/monolith.ini",
+            command => "cd ${project_dir}/current/; ./venv/bin/monolith-extract --date yesterday configs/monolith.ini >> /var/log/${aggr_name}.cron.log 2>&1",
             user    => root,
             hour    => 1,
             minute  => 0;
