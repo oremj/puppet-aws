1e530342c2ad383915a978dc2c0d787e32ecfb4b
Dean Wilson <dean.wilson@gmail.com>
2013-04-16 00:20:12 +0100

    Remove duplicated resource properties

--- modules/audit/manifests/init.pp
+++ modules/audit/manifests/init.pp
@@ -11,43 +11,29 @@ class audit {
             hasstatus => true;
     }
 
-    file {
-        "/etc/audit/auditd.conf":
-            ensure => file,
-            require => Package["audit_package"],
-            notify => Service["auditd"],
-            owner => "root",
-            group => "root",
-            mode => '0600',
-            source => "puppet:///modules/audit/auditd.conf";
-
-        "/etc/audit/audit.rules":
-            ensure => file,
-            require => Package["audit_package"],
-            notify => Service["auditd"],
-            owner => "root",
-            group => "root",
-            mode => '0600',
-            content => template("audit/audit.rules.erb");
-
-        "/etc/audisp/audispd.conf":
-            ensure => file,
-            require => Package["audit_package"],
-            notify => Service["auditd"],
-            owner => "root",
-            group => "root",
-            mode => '0600',
-            source => "puppet:///modules/audit/audispd.conf";
-
-        "/etc/audisp/plugins.d/syslog.conf":
-            ensure  => file,
-            require => Package["audit_package"],
-            notify  => Service["auditd"],
-            owner   => root,
-            group   => root,
-            mode    => '0600',
-            source  => "puppet:///modules/audit/syslog.conf";
+    File {
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0600',
+        require => Package['audit_package'],
+        notify  => Service['auditd'],
     }
+
+    file {
+        '/etc/audit/auditd.conf':
+            source => 'puppet:///modules/audit/auditd.conf';
+
+        '/etc/audit/audit.rules':
+            content => template('audit/audit.rules.erb');
+
+        '/etc/audisp/audispd.conf':
+            source => 'puppet:///modules/audit/audispd.conf';
+
+        '/etc/audisp/plugins.d/syslog.conf':
+            source  => 'puppet:///modules/audit/syslog.conf';
+    }
+
     exec {
         'restart-auditd':
             path    => '/bin:/usr/bin:/sbin:/usr/sbin',
