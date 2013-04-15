2e8eb7cf7c60726de137bbdbb647a56a46d46534
Jason Thomas <jason@lithiumfox.com>
2013-04-15 14:34:25 -0400

    Allow to disable user

--- modules/mozuser/manifests/user.pp
+++ modules/mozuser/manifests/user.pp
@@ -1,6 +1,7 @@
 define mozuser::user(
     $shell,
     $groups,
+    $ensure = present,
     $uid,
     $home = "/home/${name}",
     $shell = '/bin/bash'
@@ -8,11 +9,12 @@ define mozuser::user(
     $user_name = $name
     user {
         $user_name:
-            shell => $shell,
+            shell      => $shell,
+            ensure     => $ensure,
             managehome => true,
-            home => $home,
-            uid => $uid,
-            groups => $groups;
+            home       => $home,
+            uid        => $uid,
+            groups     => $groups;
     }
 
     Mozuser::Key <| user == $user_name |>
