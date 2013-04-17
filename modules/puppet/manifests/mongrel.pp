2a2a0a69e7e2c1e81b53ae73b79829e090df588b
Jeremiah Orem <oremj@oremj.com>
2013-04-17 14:50:31 -0700

    Add mongrel puppet.

---
+++ modules/puppet/manifests/mongrel.pp
@@ -0,0 +1,23 @@
+# Sets up mongrel proxied through nginx
+class puppet::mongrel(
+    $listen_on = '8140',
+    $mongrel_ports = ['18140',
+                      '18141',
+                      '18142',
+                      '18143'],
+    $cert_name = 'puppetmaster'
+) {
+    class {
+        'nginx':
+            nx_user => 'puppet';
+    }
+
+    nginx::config {
+        content => template('puppet/mongrel.nginx.conf');
+    }
+
+    file {
+        '/etc/sysconfig/puppetmaster':
+            content => template('puppet/puppetmaster.sysconfig.mongrel');
+    }
+}
