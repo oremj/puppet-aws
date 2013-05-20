1e9445372e32fbfaa142d27b6e4b2b7da39af3d9
Jason Thomas <jason@lithiumfox.com>
2013-05-20 15:50:11 -0400

    fix up thin for puppetmaster

--- modules/puppet/manifests/thin.pp
+++ modules/puppet/manifests/thin.pp
@@ -2,7 +2,7 @@
 class puppet::thin(
     $listen_on = '8140',
     $puppet_dir = '/etc/puppet/',
-    $puppet_socket = '/var/run/puppet/puppetmasterd.sock',
+    $puppet_socket_dir = '/var/run/puppet/',
     $puppet_workers = '4',
     $puppet_pid = '/var/run/puppet/puppetmasterd.pid',
     $puppet_rack_config = '/usr/share/puppet/ext/rack/files/config.ru',
@@ -19,7 +19,7 @@ class puppet::thin(
 
     supervisord::program {
       'puppetmaster':
-        command => "${thin} start -P ${puppet_pid} -e production --servers ${puppet_workers} --daemonize --socket ${puppet_socket}--chdir ${puppet_dir}  --user puppet --group puppet -R ${puppet_rack_config}",
+        command => "${thin} start -P ${puppet_pid} -e production --servers ${puppet_workers} --daemonize --socket ${puppet_socket_dir}/puppetmasterd.sock --chdir ${puppet_dir}  --user puppet --group puppet -R ${puppet_rack_config}",
         cwd     => '/usr/bin',
         user    => 'puppet',
         require => [
