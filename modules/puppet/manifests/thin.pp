7559dcfb21f2fdc7b8fec311d3643d788e85b236
Jason Thomas <jason@lithiumfox.com>
2013-05-20 16:34:45 -0400

    Don't use supervisor with thin

--- modules/puppet/manifests/thin.pp
+++ modules/puppet/manifests/thin.pp
@@ -5,34 +5,46 @@ class puppet::thin(
     $puppet_socket_dir = '/var/run/puppet/',
     $puppet_workers = '4',
     $puppet_pid = '/var/run/puppet/puppetmasterd.pid',
-    $puppet_rack_config = '/usr/share/puppet/ext/rack/files/config.ru',
+    $puppet_rackup = '/usr/share/puppet/ext/rack/files/config.ru',
     $cert_name = 'puppetmaster'
 ) {
-    include supervisord
     class {
         'nginx':
             nx_user => 'puppet';
-    }
-
-    $thin = '/usr/bin/thin'
-
-
-    supervisord::program {
-      'puppetmaster':
-        command => "${thin} start -P ${puppet_pid} -e production --servers ${puppet_workers} --daemonize --socket ${puppet_socket_dir}/puppetmasterd.sock --chdir ${puppet_dir}  --user puppet --group puppet -R ${puppet_rack_config}",
-        cwd     => '/usr/bin',
-        user    => 'puppet',
-        require => [
-                    Package['rubygem-thin'],
-        ],
-    }
-    nginx::config {
-        'puppet.thin':
-            content => template('puppet/thin.nginx.conf');
     }
 
     package {
         'rubygem-thin':
             ensure => present;
     }
+
+    file {
+        '/etc/init.d/puppetmaster':
+            ensure  => present,
+            content => template('puppet/init.d/puppetmaster'),
+            mode    => 0755;
+    }
+
+    file {
+        '/etc/puppetmaster.yml':
+            ensure  => present,
+            content => template('puppet/thin/puppetmaster.yml');
+    }
+
+    service {
+        'puppetmaster':
+            ensure  => running,
+            enable  => true,
+            require => [
+                        File['/etc/init.d/puppetmaster'],
+                        File['/etc/puppetmaster.yml'],
+                        Package['rubygem-thin'],
+            ],
+    }
+
+    nginx::config {
+        'puppet.thin':
+            content => template('puppet/thin.nginx.conf');
+    }
+
 }
